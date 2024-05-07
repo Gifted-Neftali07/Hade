6d493d0e5cb4b89c9388eed9ffe6e864071a68ad
Neftali-Gif <120341937+Gifted-Neftali07@users.noreply.github.com>
2024-05-06 22:53:35 -0600

    chido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent,FooterComponent,RouterModule],
+  imports: [RouterOutlet, NavbarComponent,FooterComponent,RouterModule,MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/vehiculos/vehiculos.component.ts
+++ src/app/vehiculos/vehiculos.component.ts
@@ -3,12 +3,16 @@ import { RouterModule } from '@angular/router';
 import { Vehiculos } from '../vehiculo';
 import { VehiculoService } from '../shared/vehiculo.service';
 import { CommonModule } from '@angular/common';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-vehiculos',
   standalone: true,
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule,CommonModule,MatSlideToggleModule,MatButtonToggleModule,MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './vehiculos.component.html',
   styleUrl: './vehiculos.component.css'
 })
